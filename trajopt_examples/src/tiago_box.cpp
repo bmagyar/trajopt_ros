175df99a62d5c8dd0a9f1505d3be33baff1babf0
Bence Magyar <bence.magyar.robotics@gmail.com>
2019-05-25 14:25:05 +0100

    clang-format for the win

--- trajopt_examples/src/tiago_box.cpp
+++ trajopt_examples/src/tiago_box.cpp
@@ -34,180 +34,185 @@ static tesseract_ros::KDLEnvPtr env_;             /**< Trajopt Basic Environment
 
 TrajOptProbPtr jsonMethod()
 {
-  ros::NodeHandle nh;
-  std::string trajopt_config;
+    ros::NodeHandle nh;
+    std::string trajopt_config;
 
-  nh.getParam(TRAJOPT_DESCRIPTION_PARAM, trajopt_config);
+    nh.getParam(TRAJOPT_DESCRIPTION_PARAM, trajopt_config);
 
-  Json::Value root;
-  Json::Reader reader;
-  bool parse_success = reader.parse(trajopt_config.c_str(), root);
-  if (!parse_success)
-  {
-    ROS_FATAL("Failed to load trajopt json file from ros parameter");
-  }
+    Json::Value root;
+    Json::Reader reader;
+    bool parse_success = reader.parse(trajopt_config.c_str(), root);
+    if (!parse_success)
+    {
+        ROS_FATAL("Failed to load trajopt json file from ros parameter");
+    }
 
-  return ConstructProblem(root, env_);
+    return ConstructProblem(root, env_);
 }
-void PrintHeader(std::ofstream &out) {
-  out << "Planning time (s), Trajectory length (rad), Trajectory length (m), "
-         "Smoothness, Average min distance from joint limit (normalised), "
-         "Success, Date\n";
-}
-
-void PrintResult(double planning_time, double smoothness,
-                 bool success, std::ofstream &out) {
-  std::time_t t = std::time(nullptr);
-  std::tm tm = *std::localtime(&t);
-  char *fmt = "%y.%m.%d-%H:%M:%S";
-  out << planning_time << ", 0, 0," << smoothness << ", 0," << success << ", "
-      << std::put_time(&tm, fmt) << "\n";
+void PrintHeader(std::ofstream &out)
+{
+    out << "Planning time (s), Trajectory length (rad), Trajectory length (m), "
+           "Smoothness, Average min distance from joint limit (normalised), "
+           "Success, Date\n";
 }
 
-int main(int argc, char* argv[]) {
-  std::cout << "Hello Tiago!" << std::endl;
-  std::ofstream out_file;
-  std::string filename = "/tmp/trajopr_results.csv";
-  out_file.open("/tmp/trajopr_results.csv", std::ofstream::out | std::ofstream::trunc);
-  if (out_file.is_open()){
-    PrintHeader(out_file);
-  } else {
-    std::cout << "Could not open output file" << std::endl;
+void PrintResult(double planning_time, double smoothness, bool success, std::ofstream &out)
+{
+    std::time_t t = std::time(nullptr);
+    std::tm tm = *std::localtime(&t);
+    char *fmt = "%y.%m.%d-%H:%M:%S";
+    out << planning_time << ", 0, 0," << smoothness << ", 0," << success << ", " << std::put_time(&tm, fmt) << "\n";
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello Tiago!" << std::endl;
+    std::ofstream out_file;
+    std::string filename = "/tmp/trajopr_results.csv";
+    out_file.open("/tmp/trajopr_results.csv", std::ofstream::out | std::ofstream::trunc);
+    if (out_file.is_open())
+    {
+        PrintHeader(out_file);
+    }
+    else
+    {
+        std::cout << "Could not open output file" << std::endl;
+        return 0;
+    }
+
+    ros::init(argc, argv, "glass_up_right_plan");
+    ros::NodeHandle pnh("~");
+    ros::NodeHandle nh;
+
+    // Initial setup
+    std::string urdf_xml_string, srdf_xml_string;
+    nh.getParam(ROBOT_DESCRIPTION_PARAM, urdf_xml_string);
+    nh.getParam(ROBOT_SEMANTIC_PARAM, srdf_xml_string);
+    urdf_model_ = urdf::parseURDF(urdf_xml_string);
+
+    srdf_model_ = srdf::ModelSharedPtr(new srdf::Model);
+    srdf_model_->initString(*urdf_model_, srdf_xml_string);
+    env_ = tesseract_ros::KDLEnvPtr(new tesseract_ros::KDLEnv);
+    assert(urdf_model_ != nullptr);
+    assert(env_ != nullptr);
+
+    const bool success = env_->init(urdf_model_, srdf_model_);
+    assert(success);
+
+    // Create Plotting tool
+    tesseract_ros::ROSBasicPlottingPtr plotter(new tesseract_ros::ROSBasicPlotting(env_));
+
+    // Add sphere
+    AttachableObjectPtr obj(new AttachableObject());
+    std::shared_ptr<shapes::Shape> collision_object(new shapes::Box(0.2, 0.2, 0.3));
+    // std::shared_ptr<shapes::Shape> collision_object(new shapes::Sphere(0.15));
+    Eigen::Isometry3d coll_object_pose;
+
+    coll_object_pose.setIdentity();
+    coll_object_pose.translation() = Eigen::Vector3d(0.5, 0, 0.55);
+
+    obj->name = "sphere_attached";
+    obj->visual.shapes.push_back(collision_object);
+    obj->visual.shape_poses.push_back(coll_object_pose);
+    obj->collision.shapes.push_back(collision_object);
+    obj->collision.shape_poses.push_back(coll_object_pose);
+    obj->collision.collision_object_types.push_back(CollisionObjectType::UseShapeType);
+
+    env_->addAttachableObject(obj);
+
+    AttachedBodyInfo attached_body;
+    attached_body.object_name = "sphere_attached";
+    attached_body.parent_link_name = "base_link";
+    attached_body.transform.setIdentity();
+    //  attached_body.touch_links = {}; // This element enables the attached body
+    //  to collide with other links
+
+    env_->attachBody(attached_body);
+
+    const auto environment_objects = env_->getAttachableObjects();
+    for (const auto &elem : environment_objects)
+    {
+        std::cout << "***   " << elem.first << std::endl;
+        elem.second->collision.shapes[0]->print();
+    }
+
+    // Get ROS Parameters
+    pnh.param("plotting", plotting_, plotting_);
+    pnh.param<std::string>("method", method_, method_);
+    pnh.param<int>("steps", steps_, steps_);
+
+    // // Set the robot initial state
+    // std::unordered_map<std::string, double> ipos;
+    // ipos["torso_lift_joint"] =  0.00038880942889491296;
+    // ipos["arm_1_joint"] =  1.8412129654366185;
+    // ipos["arm_2_joint"] = -0.4905036166476348;
+    // ipos["arm_3_joint"] = -3.49;
+    // ipos["arm_4_joint"] = 1.5598026200058066;
+    // ipos["arm_5_joint"] = 1.5469586195777936;
+    // ipos["arm_6_joint"] = 0.812078221806306;
+    // ipos["arm_7_joint"] = -1.3109285867767095;
+    // env_->setState(ipos);
+
+    plotter->plotScene();
+
+    // Set Log Level
+    util::gLogLevel = util::LevelInfo;
+
+    // Setup Problem
+    TrajOptProbPtr prob = jsonMethod();
+
+    // Solve Trajectory
+    ROS_INFO("glass upright plan example");
+
+    std::vector<tesseract::ContactResultMap> collisions;
+    ContinuousContactManagerBasePtr manager = prob->GetEnv()->getContinuousContactManager();
+    manager->setActiveCollisionObjects(prob->GetKin()->getLinkNames());
+    manager->setContactDistanceThreshold(0);
+
+    bool found = tesseract::continuousCollisionCheckTrajectory(*manager, *prob->GetEnv(), *prob->GetKin(),
+                                                               prob->GetInitTraj(), collisions);
+
+    ROS_INFO((found) ? ("Initial trajectory is in collision") : ("Initial trajectory is collision free"));
+
+    size_t num_exp = 3;
+    for (size_t i = 0; i < num_exp; ++i)
+    {
+        sco::BasicTrustRegionSQP opt(prob);
+        if (plotting_)
+        {
+            opt.addCallback(PlotCallback(*prob, plotter));
+        }
+
+        opt.initialize(trajToDblVec(prob->GetInitTraj()));
+        ros::Time tStart = ros::Time::now();
+        opt.optimize();
+        double duration = (ros::Time::now() - tStart).toSec();
+        ROS_ERROR("planning time: %.3f", duration);
+
+        double d = 0;
+        TrajArray traj = getTraj(opt.x(), prob->GetVars());
+        for (unsigned i = 1; i < traj.rows(); ++i)
+        {
+            for (unsigned j = 0; j < traj.cols(); ++j)
+            {
+                d += std::abs(traj(i, j) - traj(i - 1, j));
+            }
+        }
+        ROS_ERROR("trajectory norm: %.3f", d);
+        ROS_WARN_STREAM("Final trajectory: " << std::endl << traj);
+
+        if (plotting_)
+        {
+            plotter->clear();
+        }
+        collisions.clear();
+        found = tesseract::continuousCollisionCheckTrajectory(*manager, *prob->GetEnv(), *prob->GetKin(),
+                                                              prob->GetInitTraj(), collisions);
+
+        ROS_INFO((found) ? ("Final trajectory is in collision") : ("Final trajectory is collision free"));
+        PrintResult(duration, 666, !found, out_file);
+    }
+
+    out_file.close();
     return 0;
-  }
-
-  ros::init(argc, argv, "glass_up_right_plan");
-  ros::NodeHandle pnh("~");
-  ros::NodeHandle nh;
-
-  // Initial setup
-  std::string urdf_xml_string, srdf_xml_string;
-  nh.getParam(ROBOT_DESCRIPTION_PARAM, urdf_xml_string);
-  nh.getParam(ROBOT_SEMANTIC_PARAM, srdf_xml_string);
-  urdf_model_ = urdf::parseURDF(urdf_xml_string);
-
-  srdf_model_ = srdf::ModelSharedPtr(new srdf::Model);
-  srdf_model_->initString(*urdf_model_, srdf_xml_string);
-  env_ = tesseract_ros::KDLEnvPtr(new tesseract_ros::KDLEnv);
-  assert(urdf_model_ != nullptr);
-  assert(env_ != nullptr);
-
-  const bool success = env_->init(urdf_model_, srdf_model_);
-  assert(success);
-
-  // Create Plotting tool
-  tesseract_ros::ROSBasicPlottingPtr plotter(new tesseract_ros::ROSBasicPlotting(env_));
-
-  // Add sphere
-  AttachableObjectPtr obj(new AttachableObject());
-  std::shared_ptr<shapes::Shape> collision_object(new shapes::Box(0.2, 0.2, 0.3));
-  // std::shared_ptr<shapes::Shape> collision_object(new shapes::Sphere(0.15));
-  Eigen::Isometry3d coll_object_pose;
-
-  coll_object_pose.setIdentity();
-  coll_object_pose.translation() = Eigen::Vector3d(0.5, 0, 0.55);
-
-  obj->name = "sphere_attached";
-  obj->visual.shapes.push_back(collision_object);
-  obj->visual.shape_poses.push_back(coll_object_pose);
-  obj->collision.shapes.push_back(collision_object);
-  obj->collision.shape_poses.push_back(coll_object_pose);
-  obj->collision.collision_object_types.push_back(CollisionObjectType::UseShapeType);
-
-  env_->addAttachableObject(obj);
-
-  AttachedBodyInfo attached_body;
-  attached_body.object_name = "sphere_attached";
-  attached_body.parent_link_name = "base_link";
-  attached_body.transform.setIdentity();
-  //  attached_body.touch_links = {}; // This element enables the attached body
-  //  to collide with other links
-
-  env_->attachBody(attached_body);
-
-  const auto environment_objects = env_->getAttachableObjects();
-  for( const auto& elem : environment_objects)
-  {
-    std::cout << "***   " << elem.first << std::endl;
-    elem.second->collision.shapes[0]->print();
-  }
-
-  // Get ROS Parameters
-  pnh.param("plotting", plotting_, plotting_);
-  pnh.param<std::string>("method", method_, method_);
-  pnh.param<int>("steps", steps_, steps_);
-
-  // // Set the robot initial state
-  // std::unordered_map<std::string, double> ipos;
-  // ipos["torso_lift_joint"] =  0.00038880942889491296;
-  // ipos["arm_1_joint"] =  1.8412129654366185;
-  // ipos["arm_2_joint"] = -0.4905036166476348;
-  // ipos["arm_3_joint"] = -3.49;
-  // ipos["arm_4_joint"] = 1.5598026200058066;
-  // ipos["arm_5_joint"] = 1.5469586195777936;
-  // ipos["arm_6_joint"] = 0.812078221806306;
-  // ipos["arm_7_joint"] = -1.3109285867767095;
-  // env_->setState(ipos);
-
-  plotter->plotScene();
-
-  // Set Log Level
-  util::gLogLevel = util::LevelInfo;
-
-  // Setup Problem
-  TrajOptProbPtr prob = jsonMethod();
-
-  // Solve Trajectory
-  ROS_INFO("glass upright plan example");
-
-  std::vector<tesseract::ContactResultMap> collisions;
-  ContinuousContactManagerBasePtr manager = prob->GetEnv()->getContinuousContactManager();
-  manager->setActiveCollisionObjects(prob->GetKin()->getLinkNames());
-  manager->setContactDistanceThreshold(0);
-
-  bool found = tesseract::continuousCollisionCheckTrajectory(
-      *manager, *prob->GetEnv(), *prob->GetKin(), prob->GetInitTraj(), collisions);
-
-  ROS_INFO((found) ? ("Initial trajectory is in collision") : ("Initial trajectory is collision free"));
-
-  size_t num_exp = 3;
-  for (size_t i = 0; i < num_exp; ++i) {
-    sco::BasicTrustRegionSQP opt(prob);
-    if (plotting_)
-    {
-      opt.addCallback(PlotCallback(*prob, plotter));
-    }
-
-    opt.initialize(trajToDblVec(prob->GetInitTraj()));
-    ros::Time tStart = ros::Time::now();
-    opt.optimize();
-    double duration = (ros::Time::now() - tStart).toSec();
-    ROS_ERROR("planning time: %.3f", duration);
-
-    double d = 0;
-    TrajArray traj = getTraj(opt.x(), prob->GetVars());
-    for (unsigned i = 1; i < traj.rows(); ++i)
-    {
-      for (unsigned j = 0; j < traj.cols(); ++j)
-      {
-        d += std::abs(traj(i, j) - traj(i - 1, j));
-      }
-    }
-    ROS_ERROR("trajectory norm: %.3f", d);
-    ROS_WARN_STREAM("Final trajectory: " << std::endl << traj);
-
-    if (plotting_)
-    {
-      plotter->clear();
-    }
-    collisions.clear();
-    found = tesseract::continuousCollisionCheckTrajectory(
-        *manager, *prob->GetEnv(), *prob->GetKin(), prob->GetInitTraj(), collisions);
-
-    ROS_INFO((found) ? ("Final trajectory is in collision") : ("Final trajectory is collision free"));
-    PrintResult(duration, 666, !found, out_file);
-  }
-
-  out_file.close();
-  return 0;
 }
